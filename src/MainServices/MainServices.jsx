a467e2ab087e336ca6a4ed433cbe3e1a10f2db7a
Labonya <1aklimakter@gmail.com>
2023-10-07 15:55:22 +0600

    I have completed service section

--- src/MainServices/MainServices.jsx
+++ src/MainServices/MainServices.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData } from "react-router-dom";
+import Service from "./Service";
 
 
 
@@ -16,7 +17,9 @@ const MainServices = () => {
             <h1 className="text-center font-bold mt-2 text-4xl mb-2">Our Main Services</h1>
               
               <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-                    
+                  {
+                    mainData.map(data=><Service key={data.id} data={data}></Service>)
+                  }  
               </div>
 
 
